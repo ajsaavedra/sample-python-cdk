d9e8bcc0a1899ee57974141b92cd4d84607adb8a
ajsaavedra <tony.saavedra@datadoghq.com>
2021-09-23 18:17:32 -0700

    update

--- lambda/lambda_code.py
+++ lambda/lambda_code.py
@@ -3,9 +3,7 @@ import random
 
 def broken_func():
     random_num=random.randint(0,9)
-    if random_num >= 5:
-        return 'Hello, CDK! Your number is {}\n'.format(random_num)
-    return 'Hello, CDK! Your number is {}\n'.format(other_num)
+    return 'Hello, CDK! Your number is {}\n'.format(random_num)
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
