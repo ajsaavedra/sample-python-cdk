f726aae9d6bab5d511ed44a63544085fb32b276f
ajsaavedra <tony.saavedra@datadoghq.com>
2021-09-23 18:11:50 -0700

    update

--- lambda/lambda_code.py
+++ lambda/lambda_code.py
@@ -4,12 +4,8 @@ import random
 def broken_func():
     random_num=random.randint(0,9)
     if random_num >= 5:
-        try:
-            raise ValueError('Represents a hidden bug, catch this')
-            raise Exception('This is the exception you expect to not handle')
-        except ValueError as error:
-            print('Caught this error: ' + repr(error))
-    return 'Hello, CDK! Your number is {}\n'.format(random_num)
+        return 'Hello, CDK! Your number is {}\n'.format(random_num)
+    return 'Hello, CDK! Your number is {}\n'.format(other_num)
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
