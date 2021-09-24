7a344f01d0cb24650bd743c3ee195c5a0e18031e
ajsaavedra <tony.saavedra@datadoghq.com>
2021-09-23 17:38:09 -0700

    Updating func

--- lambda/lambda_code.py
+++ lambda/lambda_code.py
@@ -1,14 +1,15 @@
 import json
 import random
 
-def broken_func(event):
+def broken_func(path):
     random_num=random.randint(0,9)
     if random_num >= 5:
         try:
             raise ValueError('Represents a hidden bug, catch this')
+            raise Exception('This is the exception you expect to not handle')
         except ValueError as error:
             print('Caught this error: ' + repr(error))
-    return 'Hello, CDK! You have hit {}\n'.format(event['path'])
+    return 'Hello, CDK! You have hit {}\n'.format(path)
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
@@ -17,5 +18,5 @@ def handler(event, context):
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': broken_func(event)
+        'body': broken_func(event['path'])
     }
